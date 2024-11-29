96301a926bcded0a77fb4882c6c7839104dabc39
ArcamonUP <158064084+ArcamonUP@users.noreply.github.com>
2024-11-29 14:12:22 +0100

    Completed mandatory part
    Works for 100 numbers (650 average), but not already for 500 numbers (5 800 average, 300 higher)

---
+++ src/sort.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbaridon <kbaridon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 15:17:23 by kbaridon          #+#    #+#             */
+/*   Updated: 2024/11/29 14:01:16 by kbaridon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	has_elements_in_chunk(t_stack *stack, int chunk_limit)
+{
+	while (stack)
+	{
+		if (stack->content <= chunk_limit)
+			return (1);
+		stack = stack->next;
+	}
+	return (0);
+}
+
+static int	r_or_rr(t_stack **stack, int chunk)
+{
+	t_stack	*temp;
+	t_stack	*pos_top;
+	t_stack	*pos_bottom;
+
+	if (!stack || !(*stack))
+		return (0);
+	temp = *stack;
+	pos_top = NULL;
+	while (temp)
+	{
+		if (temp->content <= chunk)
+		{
+			if (pos_top == NULL)
+				pos_top = temp;
+			pos_bottom = temp;
+		}
+		temp = temp->next;
+	}
+	if (((*stack)->len - pos_top->len) < pos_bottom->len)
+		return (1);
+	else if (((*stack)->len - pos_top->len) == pos_bottom->len)
+		return ((pos_bottom->content < pos_top->content) + 1);
+	return (2);
+}
+
+static void	pb_in_order(t_stack **a, t_stack **b, int limit, int chunk_size)
+{
+	if ((*a)->content <= limit)
+	{
+		pb(a, b);
+		if ((*b)->content <= (limit - (chunk_size / 2)))
+		{
+			if (has_elements_in_chunk(*a, limit) && r_or_rr(a, limit) == 1)
+				rr(a, b);
+			else
+				rb(b, 1);
+		}
+	}
+	else if (r_or_rr(a, limit) == 1)
+		ra(a, 1);
+	else
+		rra(a, 1);
+}
+
+static void	move_max_to_top(t_stack **stack)
+{
+	int		max;
+	int		i;
+	t_stack	*temp;
+
+	max = maximum(*stack);
+	temp = *stack;
+	while (temp)
+	{
+		if (temp->content == max)
+		{
+			if (((*stack)->len - temp->len) <= temp->len)
+				i = 1;
+			else
+				i = 2;
+			break ;
+		}
+		temp = temp->next;
+	}
+	while ((*stack)->content != max)
+	{
+		if (i == 1)
+			rb(stack, 1);
+		else
+			rrb(stack, 1);
+	}
+}
+
+void	sort(t_stack **a, t_stack **b)
+{
+	int	chunk_size;
+	int	chunk_limit;
+
+	chunk_size = (*a)->len / 5;
+	if ((*a)->len > 200)
+		chunk_size = (*a)->len / 10;
+	while (*a && !is_sorted(*a))
+	{
+		chunk_limit = minimum(*a) + chunk_size;
+		while (has_elements_in_chunk(*a, chunk_limit))
+			pb_in_order(a, b, chunk_limit, chunk_size);
+	}
+	while (*b)
+	{
+		move_max_to_top(b);
+		pa(a, b);
+	}
+}
---
+++ src/utils.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbaridon <kbaridon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 16:34:32 by kbaridon          #+#    #+#             */
+/*   Updated: 2024/11/27 15:32:16 by kbaridon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	minimum(t_stack *stack)
+{
+	t_stack	*temp;
+	int		i;
+
+	temp = stack;
+	i = temp->content;
+	temp = stack;
+	while (temp->next)
+	{
+		if (temp->content < i)
+			i = temp->content;
+		temp = temp->next;
+	}
+	if (temp->content < i)
+		i = temp->content;
+	return (i);
+}
+
+int	maximum(t_stack *stack)
+{
+	t_stack	*temp;
+	int		i;
+
+	temp = stack;
+	i = temp->content;
+	temp = stack;
+	while (temp->next)
+	{
+		if (temp->content > i)
+			i = temp->content;
+		temp = temp->next;
+	}
+	if (temp->content > i)
+		i = temp->content;
+	return (i);
+}
+
+int	is_sorted(t_stack *a)
+{
+	t_stack	*temp;
+	int		i;
+
+	temp = a;
+	i = temp->content;
+	while (temp->next)
+	{
+		temp = temp->next;
+		if (temp->content < i)
+			return (0);
+		i = temp->content;
+	}
+	return (1);
+}
+
+t_stack	*pilenew(int content)
+{
+	t_stack	*result;
+
+	result = (t_stack *)malloc(sizeof(*result));
+	if (!result)
+		return (NULL);
+	result->content = content;
+	result->next = NULL;
+	result->len = 1;
+	return (result);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (s1[i] - s2[i]);
+	}
+	if (s2[i])
+		return (-s2[i]);
+	return (0);
+}
