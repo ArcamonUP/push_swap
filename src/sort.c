8632e7792e64a23b172ae769d26ad2319ac923b2
Kilfen Baridon <kbaridon@paul-f4Ar7s11.clusters.42paris.fr>
2024-12-04 16:38:29 +0100

    added comments

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: kbaridon <kbaridon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:17:23 by kbaridon          #+#    #+#             */
-/*   Updated: 2024/12/04 16:34:36 by kbaridon         ###   ########.fr       */
+/*   Updated: 2024/12/04 16:38:09 by kbaridon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,6 +84,10 @@ void	sort(t_stack **a, t_stack **b)
 			pb_in_order(a, b, chunk_limit, chunk_size);
 	}
 	put_to_a(a, b); //Pb : Prend parfois le chemin du min alors que ce n'est pas le plus opti
+	//test avec : 74 94 83 54 2 12 88 1 76 90 3 59 66 67 42 27 14 46 5 93 40 6 28 35 44 80 34 39 84
+	//57 20 8 49 75 100 51 68 19 52 17 48 56 22 31 97 7 87 77 11 89 29 30 32 86 33 16 95 81 38 91 58
+	//82 65 92 23 85 4 18 25 79 45 53 9 63 36 15 64 61 21 41 24 72 69 73 78 37 55 26 43 70 99 13 10 71
+	//50 47 98 60 62 96
 	while (!is_sorted(*a))
 		rra(a, 1);
 }
